c64a69ace25b01a03371db60eff1385ba06d7cb5
tzwm <wangjunzhe4444@gmail.com>
2022-09-30 16:09:22 +0800

    Update some styles and scripts

--- assets/custom.js
+++ assets/custom.js
@@ -1,19 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
+  addToggleDarkThemeListener();
+  initMermaid();
   initRemark42();
 });
 
 const initRemark42 = () => {
-  const darkSwitchesArray = document.querySelectorAll('.dark-light-switch');
-  darkSwitchesArray.forEach(darkSwitch => {
-    darkSwitch.addEventListener('click', toggleRemark42Theme);
-  });
-
   if (getCookie('nightMode')) {
     window.REMARK42.changeTheme('dark');
   }
 }
 
-const toggleRemark42Theme = () => {
+const initMermaid = () => {
+  mermaid.initialize({
+    startOnLoad: true,
+    theme: "default",
+  });
+  window.mermaid.init(undefined, document.querySelectorAll('.language-mermaid'));
+}
+
+const toggleDarkTheme = () => {
   const theme = getCookie('nightMode') ? 'dark' : 'light';
   window.REMARK42.changeTheme(theme);
+  //document.getElementById("solarized-style").setAttribute('href', '/assets/solarized_' + theme + '.css');
+}
+
+const addToggleDarkThemeListener = () => {
+  const darkSwitchesArray = document.querySelectorAll('.dark-light-switch');
+  darkSwitchesArray.forEach(darkSwitch => {
+    darkSwitch.addEventListener('click', toggleDarkTheme);
+  });
 }
